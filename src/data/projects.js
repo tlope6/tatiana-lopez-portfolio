9e83134291d53359ae2b87c82f5d3b9993e2c5f4
tlope6 <tatianamlopez27@gmail.com>
2026-02-20 11:59:09 -0600

    testing a link

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,8 +16,7 @@ export const PROJECTS = [
 
         id : 2,
         title : "Say Cheese?",
-        description :
-        "An interactive Python application that uses hand gestures and facial detection to control a live camera interface, blending computer vision, creative visuals, and data logging.",
+        description : "An interactive Python application that uses hand gestures and facial detection to control a live camera interface, blending computer vision, creative visuals, and data logging.",
         link : "https://github.com/tlope6/Say-Cheese-",
         tags :  ["Python", "OpenCV", "Computer Vision"],
         demo : null, 
@@ -26,9 +25,8 @@ export const PROJECTS = [
     {
         id : 3, 
         title : "Global Studios", 
-        description :
-        "An interactive 3D Globe website that allows users from all around the entertainment industry to connect with one another. ", 
-        link: "https://github.com/tlope6/GlobalStudios", 
+        description :"An interactive 3D Globe website that allows users from all around the entertainment industry to connect with one another. ", 
+         link: "https://github.com/tlope6/GlobalStudios", 
         tags: ["React", "Three.js", "SQL", "Supabase" ],
         image: globalStudios,
         liveLink: "https://tlope6.github.io/GlobalStudios/",
